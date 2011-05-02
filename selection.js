4db23e9a9e0daf98088cef5134b4b8f35db9fc9b
Luigi Fugaro <l.fugaro@gmail.com>
2011-05-02 22:21:09 +0200

    First commit

---
+++ selection.js
@@ -0,0 +1,8 @@
+//chrome.extension.sendRequest(window.getSelection().toString());
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getSelection")
+      sendResponse({data: window.getSelection().toString()});
+    else
+      sendResponse({}); // snub them.
+});
